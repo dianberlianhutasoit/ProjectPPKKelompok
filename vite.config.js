ade092a93e3532ab08ee6fab37e8ac713744ef22
Argifari <argifari40@gmail.com>
2026-09-11 19:20:32 +0700

    fix: compare with changes and fix konflik

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-<<<<<<< HEAD
-import { bunny } from 'laravel-vite-plugin/fonts';
-=======
->>>>>>> feature/person-1-backend
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -11,14 +7,6 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-<<<<<<< HEAD
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-=======
->>>>>>> feature/person-1-backend
         }),
         tailwindcss(),
     ],
