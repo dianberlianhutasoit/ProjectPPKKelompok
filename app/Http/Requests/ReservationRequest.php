585b6cd383dae2163314fbd845f83675066c645e
{Kayla Febrina Laura Ayu} <{email github}>
2026-09-15 20:29:20 +0700

    feat: update reservation request validation

--- app/Http/Requests/ReservationRequest.php
+++ app/Http/Requests/ReservationRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Validator; // <-- Tambahkan import ini
+use Illuminate\Validation\Validator; 
 
 class ReservationRequest extends FormRequest
 {
